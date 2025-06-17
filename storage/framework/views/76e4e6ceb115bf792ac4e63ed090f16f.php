2f051ed49c62e263cf4088921901744bc409e18d
Isabel Cañaviri Robles <isabel.yovana.2309@gmail.com>
2025-06-17 17:34:44 -0400

    Corrijo env

--- storage/framework/views/76e4e6ceb115bf792ac4e63ed090f16f.php
+++
@@ -1,2 +0,0 @@
-<img src=" <?php echo e(asset('sgc.jpg')); ?>" alt="" style="width:60px">
-<?php /**PATH C:\xampp\htdocs\proyecto-grado\resources\views\components\application-mark.blade.php ENDPATH**/ ?>
