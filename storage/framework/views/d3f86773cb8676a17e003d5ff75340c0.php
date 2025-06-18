8adbf7c67926e2779177046d1d6fb103234a7fd9
Isabel Cañaviri Robles <isabel.yovana.2309@gmail.com>
2025-06-18 09:25:46 -0400

    Fix PSR-4: renamed DerivaCorrespondenciaController

---
+++ storage/framework/views/d3f86773cb8676a17e003d5ff75340c0.php
@@ -0,0 +1,12 @@
+<div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100">
+    <img src=" <?php echo e(asset('sgc.jpg')); ?>" alt="" style="width:80px">
+    
+    
+    
+
+    <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg">
+        <?php echo e($slot); ?>
+
+    </div>
+</div>
+<?php /**PATH C:\xampp\htdocs\proyecto-grado\resources\views/components/authentication-card.blade.php ENDPATH**/ ?>
